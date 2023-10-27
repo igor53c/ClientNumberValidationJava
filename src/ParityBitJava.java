c07303731703c7846201e18ce9b9d4a40c91e282
Igor <igor53c@gmail.com>
2023-10-27 18:48:18 +0200

    Init

--- src/ParityBitJava.java
+++ src/ParityBitJava.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ParityBitJava {
+public class ParityBitJava {
+    public boolean validateClientNumber(String clientNumber) {
+
+        if (clientNumber.length() != 10) {
+            return false;
+        }
+
+        for (char ch : clientNumber.toCharArray()) {
+            if (!Character.isDigit(ch)) {
+                return false;
+            }
+        }
+
+        String binaryRepresentation = Long.toBinaryString(Long.parseLong(clientNumber));
+
+        long onesCount = binaryRepresentation.chars().filter(ch -> ch == '1').count();
+
+        return onesCount % 2 == 0;
+    }
+
 }
--- src/ParityBitJavaTest.java
+++ src/ParityBitJavaTest.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class ParityBitJavaTest {
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ParityBitJavaTest {
+
+    @Test
+    public void testValidClientNumber() {
+        ParityBitJava obj = new ParityBitJava();
+        assertTrue(obj.validateClientNumber("8456894318"));
+    }
+
+    @Test
+    public void testInvalidClientNumber() {
+        ParityBitJava obj = new ParityBitJava();
+        assertFalse(obj.validateClientNumber("3456848879"));
+    }
+
+    @Test
+    public void testNonDigitCharacter() {
+        ParityBitJava obj = new ParityBitJava();
+        assertFalse(obj.validateClientNumber("34568488a9"));
+    }
+
+    @Test
+    public void testWrongLength() {
+        ParityBitJava obj = new ParityBitJava();
+        assertFalse(obj.validateClientNumber("34568488"));
+    }
+
+    @Test
+    public void testAnotherValidClientNumber() {
+        ParityBitJava obj = new ParityBitJava();
+        assertTrue(obj.validateClientNumber("0000000000"));
+    }
 }
